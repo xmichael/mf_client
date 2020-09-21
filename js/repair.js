ae4399146898c6c9f021ae37a47ae7c0d84127a3
Michael Koutroumpas <m.koutroumpas@ed.ac.uk>
2020-09-21 16:47:48 +0300

    New webapp: "repair" to help in filling out missing data

--- js/repair.js
+++ js/repair.js
@@ -31,17 +31,6 @@ $(document).ready(function() {
 
     // Overlay layers (TMS)
 
-    /* 1930s DS data */
-    var ds = L.tileLayer('data/tiles/DS/{z}/{x}/{y}.png', {
-	tms: true,
-	opacity: 1,
-	attribution: "| This work is based on data provided through www.VisionofBritain.org.uk and uses historical Land Utilisation Survey map material which is copyright of The Land Utilisation Survey of Great Britain, 1933-49, copyright Audrey N. Clark.",
-	minZoom: 12,
-	maxNativeZoom: 14,
-	maxZoom: 20
-    });
-
-
     // Contemporary FBs
 
     var landuse_now =L.tileLayer('data/tiles/2019_land_use/{z}/{x}/{y}.png', {
@@ -52,23 +41,12 @@ $(document).ready(function() {
     });
 
     // 1840s land use tiles
-    var landuse_1840s = L.tileLayer('data/tiles/1840s_LU_new/{z}/{x}/{y}.png', {
-	minZoom: 10,
-	maxNativeZoom: 14,
+    var landuse_1840s = L.tileLayer('data/tiles/1840s_LU_repair/{z}/{x}/{y}.png', {
+	minZoom: 12,
+	maxNativeZoom: 16,
 	maxZoom: 18,
 	opacity: 1
     });
-
-    // ALC2 Grades
-    var ALC2 = L.tileLayer('data/tiles/ALC2/{z}/{x}/{y}.png', {
-	minZoom: 5,
-	attribution: "| © Crown copyright. Mapping derived from soils data © Cranfield University (NSRI) and for the Controller of HMSO 2020 © Crown copyright 2020, the Met Office. Contains OS data © Crown copyright and database right 2020. Contains Natural Resources Wales information © Natural Resources Wales and Database Right.",
-	maxNativeZoom: 13,
-	maxZoom: 20,
-	opacity: 0.5
-    });
-
-
 
     /* Dyfi Biosphere Reserver outline */
     var boundary = L.geoJSON(dataservices_boundary, {
@@ -96,22 +74,11 @@ $(document).ready(function() {
     var basemaps = {
 	"OpenStreetMap Grayscale": osmgray,
 	"No background": white
-    }
-
-
-
-    /******** LOAD OVERLAYS ******/
-    /*  var Boundary = $.ajax({url:"/data/boundary.geojson", dataType: "json",
-	success: console.log("County data successfully loaded."),
-	error: function (xhr) {
-        alert(xhr.statusText)
-	}}) */
+    };
 
     var extramaps = {
 	"Land Use <span class='text-info'>(1840s)</span>": landuse_1840s,
-	"Land Use <span class='text-info'>(Dudley Stamp 1930s)</span>": ds,
-	"Land Use <span class='text-info'>(Contemporary)</span>": landuse_now,
-	"Predictive ALC map v.2 <span class='text-info'>(Contemporary)</span>": ALC2
+	"Land Use <span class='text-info'>(Contemporary)</span>": landuse_now
     };
 
 
@@ -131,9 +98,9 @@ $(document).ready(function() {
 
     /* sequence matters for click events on map (lastest grabs clicks) */
     boundary.addTo(map);
-    //landuse_1840s.addTo(map);
+    landuse_1840s.addTo(map);
     //ds.addTo(map);
-    landuse_now.addTo(map);
+    //landuse_now.addTo(map);
     //ALC2.addTo(map);
 
 
@@ -192,63 +159,10 @@ $(document).ready(function() {
           </svg> Water
         </div>
       </div>
-    <div class="d-flex flex-column">
-        <div>
-          <h6 class="text-center">Predictive ALC grades</h6>
-        </div>
-        <div d-flex flex-row>
-          <svg width="20" height="20">
-            <circle fill="#0182fe" r="10" cx="10" cy="10"></circle>
-          </svg>
-          Excellent quality agr. land
-        </div>
-        <div>
-          <svg width="20" height="20">
-            <circle fill="#c1f9ff" r="10" cx="10" cy="10"></circle>
-          </svg> 
-          Very good quality agr. land
-        </div>
-        <div>
-          <svg width="20" height="20">
-            <circle fill="#008200" r="10" cx="10" cy="10"></circle>
-          </svg>
-          Good quality agr. land
-        </div>
-        <div>
-          <svg width="20" height="20">
-            <circle fill="#a7fca4" r="10" cx="10" cy="10"></circle>
-          </svg>
-          Moderate quality agr. land
-        </div>
-        <div>
-          <svg width="20" height="20">
-            <circle fill="#fbfa65" r="10" cx="10" cy="10"></circle>
-          </svg>
-          Poor quality agr. land
-        </div>
-        <div>
-          <svg width="20" height="20">
-            <circle fill="#b4875e" r="10" cx="10" cy="10"></circle>
-          </svg>
-          Very poor quality agr. land
-        </div>
-        <div>
-          <svg width="20" height="20">
-            <circle fill="#fec355" r="10" cx="10" cy="10"></circle>
-          </svg>
-          Non-agricultural
-        </div>
-        <div>
-          <svg width="20" height="20">
-            <circle fill="#fe6256" r="10" cx="10" cy="10"></circle>
-          </svg>
-          Urban
-        </div>
       </div>
-</div></div>
-    `
+    `;
 	var draggable = new L.Draggable(div);
-	draggable.enable()
+	draggable.enable();
 
 	return div;
     };
